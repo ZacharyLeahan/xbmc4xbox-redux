xbmc/xbmc

commit 8885f2f2207c0d3c6cd14c22754a1e52872ad56f

dvdplayer: reduce likelyhood of jumpy pts causing duplicate subtitles

xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitleLineCollection.cpp
@@ -80,7 +80,7 @@ CDVDOverlay* CDVDSubtitleLineCollection::Get(double iPts)
 {
   CDVDOverlay* pOverlay = NULL;
 
-  if (iPts < m_fLastPts)
+  if (iPts + DVD_SEC_TO_TIME(1) < m_fLastPts)
     Reset();
 
   if (m_pCurrent)

ZacharyLeahan/xbmc4xbox-redux

commit 25bf7bd19e2ae9a9d49f7ec539aa082ee8ab6096

merged: xbmc mainline db72953b - fixed: subtitles would sometime go missing on start Caused by: 8885f2f2207c0d3c6cd14c22754a1e52872ad56f

xbmc/cores/dvdplayer/DVDPlayerSubtitle.cpp
@@ -184,8 +184,11 @@ void CDVDPlayerSubtitle::Process(double pts)
     if(pts == DVD_NOPTS_VALUE)
       return;
 
-    if (pts < m_lastPts)
+    if (pts + DVD_SEC_TO_TIME(1) < m_lastPts)
+    {
       m_pOverlayContainer->Clear();
+      m_pSubtitleFileParser->Reset();
+    }
 
     if(m_pOverlayContainer->GetSize() >= 5)
       return;

xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitleLineCollection.cpp
@@ -30,7 +30,6 @@ CDVDSubtitleLineCollection::CDVDSubtitleLineCollection()
   m_pTail = NULL;
 
   m_iSize = 0;
-  m_fLastPts = DVD_NOPTS_VALUE;
 }
 
 CDVDSubtitleLineCollection::~CDVDSubtitleLineCollection()
@@ -81,9 +80,6 @@ CDVDOverlay* CDVDSubtitleLineCollection::Get(double iPts)
 {
   CDVDOverlay* pOverlay = NULL;
 
-  if (iPts + DVD_SEC_TO_TIME(1) < m_fLastPts)
-    Reset();
-
   if (m_pCurrent)
   {
     while (m_pCurrent && m_pCurrent->pOverlay->iPTSStopTime < iPts)
@@ -97,7 +93,6 @@ CDVDOverlay* CDVDSubtitleLineCollection::Get(double iPts)
 
       // advance to the next overlay
       m_pCurrent = m_pCurrent->pNext;
-      m_fLastPts = iPts;
     }
   }
   return pOverlay;
@@ -125,5 +120,4 @@ void CDVDSubtitleLineCollection::Clear()
   m_pHead    = NULL;
   m_pCurrent = NULL;
   m_iSize    = 0;
-  m_fLastPts = DVD_NOPTS_VALUE;
 }

xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitleLineCollection.h
@@ -56,7 +56,6 @@ private:
   ListElement* m_pTail;
 
   int m_iSize;
-  double m_fLastPts;
   //CRITICAL_SECTION m_critSection;
 };
 