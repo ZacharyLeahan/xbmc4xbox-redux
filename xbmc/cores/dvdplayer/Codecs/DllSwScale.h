8b50334a970de58bc971f5fd36a7cb1239b61679
antonic901 <antonicnikola6@gmail.com>
2024-04-08 01:28:42 +0200

    [569648f] added: pass cpu caps to libswscale depending on platform, since libswscale's idea of runtime cpu detection is to let the app pass it in as flags

--- xbmc/cores/dvdplayer/Codecs/DllSwScale.h
+++ xbmc/cores/dvdplayer/Codecs/DllSwScale.h
@@ -25,6 +25,17 @@ extern "C" {
 
 #include "settings/Settings.h"
 
+inline int SwScaleCPUFlags()
+{
+#if !defined(__powerpc__) && !defined(__ppc__) && !defined(__arm__)
+  return SWS_CPU_CAPS_MMX;
+#elif defined(__powerpc__) || defined(__ppc__)
+  return SWS_CPU_CAPS_ALTIVEC;
+#else
+  return 0;
+#endif
+}
+
 class DllSwScaleInterface
 {
 public:
