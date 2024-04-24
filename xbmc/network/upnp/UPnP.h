6308299935a7fe7b0ba9b4ce3aefe03d6d685d14
Nikola Antonić <59437618+antonic901@users.noreply.github.com>
2024-04-24 22:28:14 +0200

    [UPnP] Code shuffle #1238 (#152)

--- xbmc/network/upnp/UPnP.h
+++ xbmc/network/upnp/UPnP.h
@@ -23,23 +23,21 @@
 
 #pragma once
 
-/* forward declarations */
+#include "utils/StdString.h"
+
 class PLT_UPnP;
 class PLT_SyncMediaBrowser;
+class PLT_MediaObject;
+class PLT_MediaItemResource;
+
+namespace UPNP
+{
+
 class CDeviceHostReferenceHolder;
 class CCtrlPointReferenceHolder;
 class CRendererReferenceHolder;
 class CUPnPRenderer;
 class CUPnPServer;
-class PLT_MediaObject;
-class PLT_MediaItemResource;
-
-namespace MUSIC_INFO {
-class CMusicInfoTag;
-}
-
-class CVideoInfoTag;
-
 
 class CUPnP
 {
@@ -60,14 +58,6 @@ public:
     bool StartRenderer();
     void StopRenderer();
     void UpdateState();
-
-    // methods
-    static int PopulateTagFromObject(MUSIC_INFO::CMusicInfoTag& tag,
-                                     PLT_MediaObject&           object,
-                                     PLT_MediaItemResource*     resource = NULL);
-    static int PopulateTagFromObject(CVideoInfoTag&             tag,
-                                     PLT_MediaObject&           object,
-                                     PLT_MediaItemResource*     resource = NULL);
 
     // class methods
     static CUPnP* GetInstance();
@@ -93,3 +83,5 @@ private:
     static CUPnP* upnp;
     static bool   broadcast;
 };
+
+} /* namespace UPNP */
